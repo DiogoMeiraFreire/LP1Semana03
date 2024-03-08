fd7691bdf1e9647515485e5b48b0a6030866a44b
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-08 19:44:16 +0000

    Added HeroPerksEnum

---
+++ HeroPerks/HeroPerksEnum.cs
@@ -0,0 +1,10 @@
+namespace HeroPerks.Enum
+{
+    public enum HeroPerksEnum
+    {
+        Stealth = 1 << 0,
+        DoubleJump = 1 << 1,
+        WaterBreathing = 1 << 2,
+        AutoHeal = 1 << 3
+    }
+}
--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HeroPerks.Enum;
 
 namespace HeroPerks
 {
