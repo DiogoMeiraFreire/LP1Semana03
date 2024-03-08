df34fd326b13ab7fa323cb1ed5771c8e4cb57125
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-08 19:32:56 +0000

    Added completed ArrayMult

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -4,9 +4,32 @@ namespace ArrayMult
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float[,] array1 = new float[2, 2];
+            float[,] array2 = new float[2, 1];
+           
+            array1[0,0] = float.Parse(args[0]);
+            array1[0,1] = float.Parse(args[1]);
+            array1[1,0] = float.Parse(args[2]);
+            array1[1,1] = float.Parse(args[3]);
+
+            array2[0,0] = float.Parse(args[4]);
+            array2[1,0] = float.Parse(args[5]);   
+        
+            
+            for(int i = 0; i < 2; i++)
+            {
+                float answer = 0F; 
+                for(int j = 0; j < 2 ; j++)
+                {
+                    answer += array1[i, j] * array2[j, 0];
+                    
+                }
+                Console.WriteLine(CalcResult);
+            }
+
+           
         }
     }
 }
