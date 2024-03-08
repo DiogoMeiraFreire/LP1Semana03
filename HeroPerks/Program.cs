b24ae75b2cb100506fdad6139f6c56bb30ace405
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-08 21:43:11 +0000

    Completed HeroPerks

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -7,7 +7,58 @@ namespace HeroPerks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string playerPerks = "";
+            HeroPerksEnum perksEnum = 0;
+
+            foreach (string s in args)
+            {
+                playerPerks = s;
+            }
+
+            for (int i = 0; i < playerPerks.Length; i++)
+            {
+                if (playerPerks[i] == 'w')
+                {
+                    perksEnum ^= HeroPerksEnum.WaterBreathing;
+                }
+                else if (playerPerks[i] == 's')
+                {
+                    perksEnum ^= HeroPerksEnum.Stealth;
+                }
+                else if (playerPerks[i] == 'a')
+                {
+                    perksEnum ^= HeroPerksEnum.AutoHeal;
+                }
+                else if (playerPerks[i] == 'd')
+                {   
+                    perksEnum ^= HeroPerksEnum.DoubleJump;
+                }
+                else if (playerPerks[i] != 'w' || playerPerks[i] != 's'||
+                    playerPerks[i] != 'a'|| playerPerks[i] != 'd')
+                    {
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                    }
+            }
+            if (perksEnum != 0)
+                Console.WriteLine(perksEnum);
+
+            if ((perksEnum & HeroPerksEnum.Stealth) == HeroPerksEnum.Stealth 
+            && (perksEnum & HeroPerksEnum.DoubleJump) == HeroPerksEnum.DoubleJump)
+            {
+                Console.WriteLine("Silent jumper!");
+            }
+
+            if ((perksEnum & HeroPerksEnum.AutoHeal) != HeroPerksEnum.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }
+
+            if (perksEnum == 0)
+            {
+                Console.WriteLine("No perks at all!");
+            }
+
         }
     }
 }
