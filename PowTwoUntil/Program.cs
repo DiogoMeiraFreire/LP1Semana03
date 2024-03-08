3b409df5fbd67d861d5c5f6bb3b2af309380b47d
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-08 22:09:48 +0000

    Added PowersOf2Until5 being called 2 times

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,12 +6,15 @@ namespace PowTwoUntil
     {
         static void Main(string[] args)
         {
-
+            for (int j = 0; j < 2; j++)
+            {
+                PowersOf2Until5();
+            }
         }
 
         private static void PowersOf2Until5()
         {
-            for (int i = 0; i <= (1 << 5); i = i << 1)
+            for (int i = 1; i <= (1 << 5); i = i << 1)
             {
                 Console.WriteLine(i);
             }
